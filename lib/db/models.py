e68b89e54596c6486d64d45b6a96106a5f2b31ab
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-08-30 23:07:59 +0300

    creates many-to-many relationship for spark and collection
     - adds context helper to the Spark model

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,8 +1,16 @@
 from sqlalchemy import func
-from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
+
+spark_collection = Table(
+    "spark_collections",
+    Base.metadata,
+    Column("spark_id", Integer(), ForeignKey("sparks.id")),
+    Column("collection_id", Integer(), ForeignKey("collections.id"))
+)
 
 class Context(Base):
     __tablename__ = "contexts"
@@ -20,3 +28,17 @@ class Spark(Base):
     created_at = Column(DateTime(), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(), onupdate=func.now())
     context_id = Column(Integer(), ForeignKey("contexts.id"))
+
+    context = relationship("Context", backref=backref("spark"))
+    collections = relationship("Collection", secondary=spark_collection, back_populates="sparks")
+
+class Collection(Base):
+    __tablename__ = "collections"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(Text(), nullable=False)
+    description = Column(Text())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    sparks = relationship("Spark", secondary=spark_collection, back_populates="collections")

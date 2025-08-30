a2bb5a76b1adf74d089d3711d49f5933d0bba54b
Navis Ochieng <navis.ochieng@student.moringaschool.com>
2025-08-30 22:49:43 +0300

    adds spark model

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import func
-from sqlalchemy import Column, Integer, Text, DateTime
+from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -11,3 +11,12 @@ class Context(Base):
     working_directory = Column(Text(), nullable=False)
     project_name = Column(Text())
     created_at = Column(DateTime(), server_default=func.now())
+
+class Spark(Base):
+    __tablename__ = "sparks"
+
+    id = Column(Integer(), primary_key=True)
+    content = Column(Text(), nullable=False)
+    created_at = Column(DateTime(), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(), onupdate=func.now())
+    context_id = Column(Integer(), ForeignKey("contexts.id"))
